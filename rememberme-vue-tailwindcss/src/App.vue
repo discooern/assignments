a0c5fff6386bb6a12fe7cc57166ea870d7468f68
discooern <jona834n@edu.campusvejle.dk>
2021-08-19 14:35:06 +0200

    added folder

---
+++ rememberme-vue-tailwindcss/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<section class="dark:bg-black w-4/12 p-10 mx-auto mt-12 rounded">
+  <Header></Header>
+  <Main></Main> 
+  </section>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main
+  }
+}
+</script>
+
+<style>
+  
+</style>
