a5779b600555d68d52df51379b354a6deb536170
discooern <72135961+discooern@users.noreply.github.com>
2021-08-19 14:52:29 +0200

    d
    added

---
+++ rememberme-vue-tailwindcss/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="justify-between flex pb-4">
+    <h1 class="text-gray-400 font-semibold">REMEMBER ME</h1>
+    <button class="text-blue-500 bg-blue-100 rounded w-20 h-8 transition duration-200 ease-in-out hover:bg-blue-200"><strong>+</strong> Add</button>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ rememberme-vue-tailwindcss/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li class="bg-white my-5 p-5 rounded-xl flex flex-col">
+      <div class="flex justify-between mb-2">
+        <h2 class="text-black font-semibold text-xl">Official Guide</h2>
+        <button class="text-gray-400 transition duration-200 ease-in-out hover:text-gray-600 hover:shadow-sm rounded-3xl">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </button>
+      </div>
+      <p class="text-gray-500 font-semibold">The official Vue.js documentation.</p>
+    </li>
+    <li class="bg-white p-5 rounded-xl">
+      <div class="flex justify-between mb-2">
+        <h2 class="text-black font-semibold text-xl">Google</h2>
+        <button class="text-gray-400 transition duration-200 ease-in-out hover:text-gray-600 hover:shadow-sm rounded-3xl">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </button>
+      </div>
+      <p class="text-gray-500 font-semibold">Learn go google...</p>
+    </li>
+  </ul>
+  <span class="border-dashed border-2 block text-center p-5 text-gray-300 font-semibold my-5">Add resource</span>    
+</template>
